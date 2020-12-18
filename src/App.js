d367d4107d828aac67272b3cc0fb06f1ab45d39f
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-12-18 22:37:25 +0100

    .

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ export default function App() {
           {status === "OFF" ? "OFF" : "ON"}
         </button>
       </div>
-      <footer>(original, without useReducer)</footer>
+      <footer>original, without useReducer</footer>
     </div>
   );
 }
